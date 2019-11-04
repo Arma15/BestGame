034ab22accaa34b96fb0bab46f9c2de0a2ceb47c
drayarms <drayarms@yahoo.com>
2019-11-03 23:16:18 -0800

    Spider wave modified
    Modified spider wave such that spiders now appear on the scene at regular intervals, each at a random location near two of the walls of the room.

--- Assets/Scripts/SpiderWave.cs
+++ Assets/Scripts/SpiderWave.cs
@@ -4,38 +4,50 @@ using UnityEngine;
 
 public class SpiderWave : MonoBehaviour
 {
+    public System.Random rnd = new System.Random();
+    int spiderTimer;
+    int currentSpider;
+    int spiderInitiationFreq;
 
-    int timer;
-    //Object spiderPrefab = AssetDatabase.LoadAssetAtPath("Assets/Models/18754_Spider_in_defensive_stance_V1", typeof(GameObject));
-    //Object spiderPrefab = Resources.Load("Assets/Models/18754_Spider_in_defensive_stance_V1", typeof(GameObject));
-    //Object spiderPrefab = Resources.Load("Assets/Models/18754_Spider_in_defensive_stance_V1");
     public GameObject spiderPrefab;
-    GameObject[] spiders = new GameObject[20];
+    GameObject[] spiders = new GameObject[40];
     //GameObject spider1;
     
 
     // Start is called before the first frame update
     void Start()
     {
-        timer = 0;
+        spiderTimer = 0;
+        currentSpider = 0;
+        currentSpider = 0;
+        spiderInitiationFreq = 150;
 
-        //GameObject spider1 = Instantiate(spiderPrefab, new Vector3(-1025, -349, -2573), Quaternion.Euler(-76.095f, -0.195f, -31.17f)) as GameObject;
-        //spider1 = Instantiate(spiderPrefab, new Vector3(-1025, -359, -2573), Quaternion.Euler(-76.095f, -0.195f, -31.17f)) as GameObject;
-        spiders[0] = Instantiate(spiderPrefab, new Vector3(-1025, -359, -2573), Quaternion.Euler(-76.095f, -0.195f, -31.17f)) as GameObject;
-        spiders[1] = Instantiate(spiderPrefab, new Vector3(-990, -359, -2573), Quaternion.Euler(-76.095f, -0.195f, -31.17f)) as GameObject;
-        spiders[2] = Instantiate(spiderPrefab, new Vector3(-1100, -359, -2573), Quaternion.Euler(-76.095f, -0.195f, -31.17f)) as GameObject;
-
-        //spider1.transform.position = Vector3(-1025, -349, -2573);
-        //spider1.transform.localScale = new Vector3(10, 10, 10);
-        spiders[0].transform.localScale = new Vector3(10, 10, 10);
-        spiders[1].transform.localScale = new Vector3(10, 10, 10);
-        spiders[2].transform.localScale = new Vector3(10, 10, 10);
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += 1;
+
+        spiderTimer += 1;
+        spiderTimer = spiderTimer % spiderInitiationFreq;
+
+        if(spiderTimer == 0)
+        {
+            float posX = Random.Range(-1542, -738);
+            float posY = Random.Range(-369, -67);
+
+            float[] zRoomBounds = new float[2] { -2688, -1977 };
+            int indexZ = rnd.Next(0, 2);//Random # btw 0 and 1
+            Debug.Log(indexZ);
+            float posZ = zRoomBounds[indexZ];
+
+            spiders[currentSpider] = Instantiate(spiderPrefab, new Vector3(posX, posY, posZ), Quaternion.Euler(-76.095f, -0.195f, -31.17f)) as GameObject;
+            spiders[currentSpider].transform.localScale = new Vector3(5, 5, 5);
+
+            currentSpider++;
+        }
+
+        /*timer += 1;
         timer = timer % 999999;
 
         if(timer == 1000)
@@ -51,7 +63,7 @@ public class SpiderWave : MonoBehaviour
         if (timer == 3000)
         {
             Destroy(spiders[2]);
-        }
+        }*/
         //Debug.Log("timer: " + timer);
     }
 }
