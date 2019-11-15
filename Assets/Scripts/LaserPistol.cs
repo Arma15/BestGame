7c6011b585882dd91f2b1d63c828db7b27a1c56b
drayarms <drayarms@yahoo.com>
2019-11-14 21:32:02 -0800

    Bye bye spider
    Can now kill spiders with click of a mouse. Need to add a ray when a shot is fired though. Also, once player moves, ray isn't being cast in the right direction(need fixing).

--- Assets/Scripts/LaserPistol.cs
+++ Assets/Scripts/LaserPistol.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class LaserPistol : MonoBehaviour
 {
-
-
+    AudioSource laserBurst;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -21,21 +21,33 @@ public class LaserPistol : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
-            if (Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity))
+
+            //Play sound
+            laserBurst = GetComponent<AudioSource>();
+            laserBurst.Play(0);
+
+
+            //if (Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity))
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
             {
                 //Debug.Log("Hit " + hit.collider.gameObject.name, hit.collider.gameObject);
-                Debug.Log("Hit " + hit.collider.gameObject.name);
+                //Debug.Log("Hit " + hit.collider.gameObject.name+" at distance " + hit.distance);
 
 
 
                 //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
                 //Debug.DrawLine(new Vector3(0,0,0), new Vector3(1,1,1), Color.white, 100f);
                 //Debug.DrawLine(transform.position, transform.forward, Color.white, 100f);
-
                 //Debug.DrawRay(transform.position, transform.forward, Color.green, 1000f);
 
-                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 10000, Color.yellow);
-                //Destroy(hit.collider.gameObject);
+
+
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 10000, Color.red);
+
+
+                //Kill spider
+                Destroy(hit.collider.gameObject);
+                //numSpiders--;
             }
         }
 
